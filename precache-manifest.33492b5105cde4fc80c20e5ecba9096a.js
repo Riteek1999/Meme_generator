93b8ae759d701c1b759c02f69ceb2eea7cebdec0
Riteek <riteek2000@gmail.com>
2020-07-01 15:43:44 +0530

    Updates

---
+++ precache-manifest.33492b5105cde4fc80c20e5ecba9096a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0d61c1820d40e3964870c4180d84a0",
+    "url": "/Meme_generator/index.html"
+  },
+  {
+    "revision": "f20f02804f520f8f2e32",
+    "url": "/Meme_generator/static/css/main.6dc12aea.chunk.css"
+  },
+  {
+    "revision": "7ed7bcd7abcb69fd597c",
+    "url": "/Meme_generator/static/js/2.c762ba29.chunk.js"
+  },
+  {
+    "revision": "462f991c1d2ef16102b314f225132ac4",
+    "url": "/Meme_generator/static/js/2.c762ba29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20f02804f520f8f2e32",
+    "url": "/Meme_generator/static/js/main.aedfc3fa.chunk.js"
+  },
+  {
+    "revision": "79823f02daabb8264166",
+    "url": "/Meme_generator/static/js/runtime-main.d2a6bf18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Meme_generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
